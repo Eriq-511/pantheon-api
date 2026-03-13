7dbc10b3c9652ac3a17a9aba46e364ce94268e7b
Eriq-511 <2023bit216@std.must.ac.ug>
2026-03-13 19:23:17 +0300

    Add root endpoint for health/status check

---
+++ src/main/java/com/cms/controller/RootController.java
@@ -0,0 +1,12 @@
+package com.cms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/")
+    public String root() {
+        return "Pantheon API is running.";
+    }
+}
